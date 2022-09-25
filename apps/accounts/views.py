1b288221ba83b2e410d02690581ae628f954d9b0
M.maeda <petit.chat.0509@gmail.com>
2022-09-25 22:42:20 +0900

    認証機能実装完了

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.contrib.auth import views
+from apps.accounts.form import LoginForm, PasswordResetForm, SetPasswordForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class LoginView(views.LoginView):
+    form_class = LoginForm
+    redirect_authenticated_user = True
+
+    def get_redirect_url(self):
+        url = super().get_redirect_url()
+        # ログイン後のリダイレクト先
+        # if url == '':
+        #     url = reverse_lazy('')
+
+        return url
+
+
+class LogoutView(views.LogoutView):
+    next_page = 'accounts:login'
+
+
+class PasswordResetView(views.PasswordResetView):
+    template_name = 'pw_reset/form.html'
+    email_template_name = 'pw_reset/email.html'
+    success_url = reverse_lazy('accounts:pw_reset_done')
+    subject_template_name = 'pw_reset/email_subject.txt'
+    form_class = PasswordResetForm
+
+    def post(self, request, *args, **kwargs):
+        request.session['reset_email'] = request.POST['email']
+        super().post(self, request, *args, **kwargs)
+
+        return HttpResponseRedirect(self.success_url)
+
+
+class PasswordResetDoneView(views.PasswordResetDoneView):
+    template_name = 'pw_reset/done.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['reset_email'] = self.request.session.get('reset_email')
+
+        return context_data
+
+
+class PasswordResetConfirmView(views.PasswordResetConfirmView):
+    template_name = 'pw_reset/confirm.html'
+    form_class = SetPasswordForm
+    success_url = reverse_lazy('accounts:pw_reset_complete')
+
+
+class PasswordResetCompleteView(views.PasswordResetCompleteView):
+    template_name = 'pw_reset/complete.html'
