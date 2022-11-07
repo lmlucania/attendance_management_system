5dd5ef9617b3eec79053e8a8d0b3677a68538a8e
M.maeda <petit.chat.0509@gmail.com>
2022-11-08 06:15:43 +0900

    [timecard]ベースビュー作成

---
+++ apps/timecard/views/__init__.py
@@ -0,0 +1,4 @@
+from .dashboard import DashboardView
+from .timecard import TimeCardListView, TimeCardCreateView, TimeCardUpdateView, TimeCardDeleteView
+
+__all__ = ['DashboardView', 'TimeCardListView', 'TimeCardCreateView', 'TimeCardUpdateView', 'TimeCardDeleteView']
---
+++ apps/timecard/views/base.py
@@ -0,0 +1,48 @@
+from django.http import Http404, HttpResponseRedirect
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+
+
+class TemplateView(LoginRequiredMixin, generic.TemplateView):
+    login_url = reverse_lazy('accounts:login')
+
+
+class ListView(LoginRequiredMixin, generic.ListView):
+
+    def get_template_names(self):
+        return ['%s/list.html' % (self.object_list.model._meta.app_label)]
+
+
+class CreateView(LoginRequiredMixin, generic.CreateView):
+
+    def get_template_names(self):
+        return ['%s/form.html' % (self.model._meta.app_label)]
+
+
+class UpdateView(LoginRequiredMixin, generic.UpdateView):
+
+    def get_template_names(self):
+        return ['%s/form.html' % (self.model._meta.app_label)]
+
+
+class DeleteView(LoginRequiredMixin, generic.DeleteView):
+
+    def get_object(self, queryset=None):
+        try:
+            return super().get_object(queryset)
+        except Http404:
+            pass
+
+    def delete(self, request, *args, **kwargs):
+        _object = self.get_object()
+        if _object:
+            _object.delete()
+
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        if self.success_url:
+            return self.success_url
+        else:
+            super().get_success_url()
