f3f448591c147e2088718281cd98095eb987dc5c
M.maeda <petit.chat.0509@gmail.com>
2023-03-24 23:29:24 +0900

    [timecard]ダッシュボード画面の修正

--- apps/timecard/forms/__init__.py
+++ apps/timecard/forms/__init__.py
@@ -1,4 +1,4 @@
 from .base import BaseForm
-from .timecard import TimeCardForm, TimeCardSearchForm, TimeCardFormSet
+from .timecard import TimeCardForm, TimeCardSearchForm, TimeCardFormSet, UploadFileForm
 
-__all__ = ['BaseForm', 'TimeCardForm', 'TimeCardSearchForm', 'TimeCardFormSet']
+__all__ = ['BaseForm', 'TimeCardForm', 'TimeCardSearchForm', 'TimeCardFormSet', 'UploadFileForm']
--- apps/timecard/views/dashboard.py
+++ apps/timecard/views/dashboard.py
@@ -10,46 +10,54 @@ from apps.timecard.models import TimeCard
 class DashboardView(TemplateView):
     template_name = 'dashboard.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        self.today = timezone.datetime.today().astimezone(timezone.get_default_timezone()).replace(
+            hour=0, minute=0, second=0, microsecond=0)
+        self.is_promoted = TimeCard.objects.filter(stamped_time__gte=self.today + relativedelta(day=1),
+                                                   stamped_time__lt=self.today + relativedelta(
+                                                       months=1, day=1) - relativedelta(days=1),
+                                                   user=self.request.user).exclude(state=TimeCard.State.NEW).exists()
+
+        return super().dispatch(request, *args, **kwargs)
+
     def post(self, request, *args, **kwargs):
 
         if 'mode' not in self.request.POST:
             return super().get(request, *args, **kwargs)
 
-        stamping_kind = self._validate_stamping(self._convert_stamping_kind(self.request.POST['mode']))
-        if stamping_kind:
+        if self.is_promoted:
+            self.request.session['error'] = '締め処理後のため、打刻できません'
+            return super().get(request, *args, **kwargs)
+
+        stamping_kind = self._convert_stamping_kind(self.request.POST['mode'])
+        if not self._is_stamped_today(stamping_kind):
             TimeCard.objects.create(user=self.request.user, kind=stamping_kind)
 
         return redirect(reverse('timecard:dashboard'))
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # context['can_click_in'] = ''
-        # context['can_click_reset'] = 'disabled'
-        #
-        # if self._latest_record_stamping_kind() == TimeCard.Kind.IN:
-        #     context['can_click_in'] = 'disabled'
-        #     context['can_click_reset'] = ''
+        context['is_promoted'] = self.is_promoted
+        context['btn_in_attr'] = self._get_btn_attr(TimeCard.Kind.IN)
+        context['btn_out_attr'] = self._get_btn_attr(TimeCard.Kind.OUT)
+        context['btn_enter_break_attr'] = self._get_btn_attr(TimeCard.Kind.ENTER_BREAK)
+        context['btn_end_break_attr'] = self._get_btn_attr(TimeCard.Kind.END_BREAK)
 
         return context
 
     def _convert_stamping_kind(self, mode):
-        if type(mode) != str:
-            return
-
         try:
             return TimeCard.Kind[mode.upper()]
 
-        except KeyError:
+        except:
             return
 
-    def _validate_stamping(self, stamping_kind):
-        today = timezone.datetime.today().astimezone(timezone.get_default_timezone()).date()
-        today_query = TimeCard.objects.filter(user=self.request.user, stamped_time__gte=today,
-                                              stamped_time__lt=today + relativedelta(days=1))
-        if today_query.filter(kind=stamping_kind).exists():
-            return
+    def _is_stamped_today(self, stamping_kind):
+        today_qs = TimeCard.objects.filter(user=self.request.user, stamped_time__gte=self.today,
+                                           stamped_time__lt=self.today + relativedelta(days=1))
 
-        if stamping_kind == TimeCard.Kind.OUT:
-            return today_query.filter(kind=TimeCard.Kind.IN).exists()
+        return today_qs.filter(kind=stamping_kind).exists()
 
-        return stamping_kind
+    def _get_btn_attr(self, stamped_kind):
+        if self.is_promoted or self._is_stamped_today(stamped_kind):
+            return 'disabled'
